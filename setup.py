4e570d0fe6a2e8b3b28077ffe90c2779a68cec86
John James <john.james.ai.studio@gmail.comgit config --global user.email john.james.ai.studio@gmail.comgit config --global user.email john.james.ai.studio@gmail.com>
2022-02-12 17:18:31 -0500

    wsl

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deep Learning for Conversion Rate Prediction and Analytics',
+    author='John James',
+    license='BSD-3',
+)
